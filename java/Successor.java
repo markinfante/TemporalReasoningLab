124a7935ec52db5fe53909eb3a707ea21d61ddec
ckodonnell <71301178+ckodonnell@users.noreply.github.com>
2020-09-17 16:07:16 -0400

    Update Successor.java

--- java/Successor.java
+++ java/Successor.java
@@ -1,12 +1,12 @@
 package java;
 
-/* Successor.java
+/* Edge.java
  * Author: Mark Infante
  *      A successor is a 3-tuple (X, d, Y), where X is a start node, Y is an end node,
  *      and d is delta, the weight of the edge from X --> Y. 
 */
 
-public class Successor{
+public class Edge{
 
     private Integer startNode;
     private Integer endNode;
@@ -14,7 +14,7 @@ public class Successor{
 
     // @params 
     // successor = (X, d, Y) = (Integer, Double, Integer)
-    public Successor(Integer startNode, Double weight, Integer endNode){
+    public Edge(Integer startNode, Double weight, Integer endNode){
         this.startNode = startNode;
         this.weight = weight;
         this.endNode = endNode; 
