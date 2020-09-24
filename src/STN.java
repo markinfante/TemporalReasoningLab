6b3a5aa71eb36971406e5ea1956229461993e719
ckodonnell <codonnell@vassar.edu>
2020-09-24 17:26:34 -0400

    updated STN

--- src/STN.java
+++ src/STN.java
@@ -1,54 +1,106 @@
-package src;
-
 import java.util.*;
 
-/** STN.java
+/* STN.java
+ * Author: Mark Infante
  *      A Simple Temporal Network extending TemporalNetwork
  *      Allows for creation of directed, weighted graph  
- *  @author Mark Infante
 */
 
 public class STN extends TemporalNetwork{
     
-    private ArrayList<? extends List <Integer>> successorsMatrix;   // A 2D matrix of nodes and their successors 
-    private ArrayList<? extends List <Integer>> predsMatrix; 
-    private ArrayList<Integer> numSuccessors;            // Number of successor nodes for corresponding node index
-    private ArrayList<? extends List <Integer>> edgesMatrix;        // A 2D matrix of edge weights for corresponding node index pair
-    private ArrayList<? extends List <Integer>> distanceMatrix; 
+    private List<Map<Integer, Edge>> successors;   // A 2D vector that holds information about a node's successors.
+    private List<Integer> numSuccessors;            // Number of successor nodes for corresponding node index
+    private List<ArrayList <Double>> edgesMatrix;        // A 2D matrix of edge weights for corresponding node index pair
 
     // Default constructor for simple temporal network
     public STN(){
         setNetType(TemporalNetworks.STN);
-        successorsMatrix = new ArrayList<ArrayList<Integer>>();
         numSuccessors = new ArrayList<Integer>();
-        edgesMatrix = new ArrayList<ArrayList<Integer>>();
+        successors = new Vector<Map<Integer, Edge>>(); //iniitialize successors as a Vector
+        edgesMatrix = new ArrayList<ArrayList<Double>>();
+        
+    }
+    
+    public void init(){
+        int tps = super.getNumTimePoints(); //populate the spaces in the vector, based on the number of timepoints
+        for (int i = 0; i < tps; i++) {
+            successors.add(i, new HashMap<Integer, Edge>()); 
+            numSuccessors.add(0);
+            edgesMatrix.add(i, new ArrayList<Double>());
+            for (int k = 0; k < tps; k++)
+            {
+                edgesMatrix.get(i).add(0.0);
+            }
+        }
+        
+        
+        //System.out.println(edgesMatrix);
     }
 
-    // @params A list of edge 3-tuples (X, d, Y) where X is start node,
+    /* @params A list of edge 3-tuples (X, d, Y) where X is start node,
     //         Y is end node, and d is the edge weight.
     public STN(ArrayList<Edge> graph){
         // TODO: convert a list of 3-tuples (X, d, Y) into a STN.
-    }
+        
+        
+    }*/
 
-    @Override
-    public void addEdge(Edge edge){
+    @Override 
+    // @params An existing STN s and an Edge object edge consisting of a source, destination, and a weight.
+    public void addEdge(Edge edge){ 
         // TODO: all
+        Integer x = edge.getStart();
+        Integer y = edge.getEnd();
+        Double d = edge.getWeight();
+        Edge e = successors.get(x).get(y);
+        
+        if (e == null){ //if an edge doesn't exist in successors, input the given edge argument. also increment numsuccessors, since we are adding a new edge
+            successors.get(x).put(y, edge);
+            numSuccessors.set(x, numSuccessors.get(x)+1);
+            edgesMatrix.get(x).add(y, d);
+        }
+        else if (e != null && (e.getWeight() > d)){ //else if new edge gives a shorter path from x->y than the old edge, replace the old edge with the new one
+            successors.get(x).put(y, edge);
+            edgesMatrix.get(x).set(y, d);
+        }
     }
 
     @Override
     public void removeEdge(Edge edge){
-        // TODO: all
+        Integer x = edge.getStart();
+        Integer y = edge.getEnd();
+        Edge e = successors.get(x).get(y);
+        if (edge == null){
+            System.out.println("Cannot remove edge because edge does not exist.");
+            return;
+        }
+        else {
+            successors.get(x).put(y, null);
+            numSuccessors.set(x, numSuccessors.get(x)-1);
+            edgesMatrix.get(x).set(y, 0.0);
+        }
+        return;
+    }
+    
+    public Edge getEdge(Integer src, Integer dest){
+        return successors.get(src).get(dest);
     }
 
     @Override
     void addNode() {
         // TODO: all
+        return;
     }
 
     @Override
     void removeNode() {
         // TODO Auto-generated method stub
+        return;
 
     }
     
+    public String toString()
+    {
+        return successors + "";
+    }
 }
