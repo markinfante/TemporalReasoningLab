c40bc698f10936c3bc3f62d1cb56e6a21d8a4b11
ckodonnell <codonnell@vassar.edu>
2020-10-05 10:41:22 -0400

    added one line to initialize distance matrix

--- src/STNwithDistance.java
+++ src/STNwithDistance.java
@@ -27,7 +27,8 @@ public class STNWithDistance extends STN{
         successors = new ArrayList<Map<Integer, Edge>>(); //iniitialize successors as a Vector
         edgesMatrix = new ArrayList<ArrayList<Edge>>();
         this.network = network;
-        distanceMatrix = new DistanceMatrix(generatorType, this.network);
+        distanceMatrix = new DistanceMatrix(generatorType, this.network);w
+        distanceMatrix.generateMatrix();
     }
 
     /**
