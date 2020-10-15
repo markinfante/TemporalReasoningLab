af04e1173235e50832c8ed2ba5a9aea3244ae90a
mjaklitsch <mjaklitsch123@gmail.com>
2020-10-15 14:06:29 -0400

    TwoPhasePropagation finished and commented. Untested

--- src/TwoPhasePropagation.java
+++ src/TwoPhasePropagation.java
@@ -1,55 +1,99 @@
 package src;
 
-// import java.util.ArrayList;
+import java.util.*;
 
 public class TwoPhasePropagation {
-    private STNWithDistance networkWithDistance;
+    private STN network;
     private DistanceMatrix outputMatrix;
 
-    public TwoPhasePropagation(TemporalNetwork TN){
-        networkWithDistance = new STNWithDistance(TN, MatrixGeneratorType.JOHNSONS, MatrixIncrementorType.TWOPHASE);
-        
-        // outputMatrix = networkWithDistance.getDistanceMatrix();
-        // Edge tEdge;  // temp edge
-        outputMatrix = networkWithDistance.getDistanceMatrix();
-
-        // Copied from Mark's code in FloydWarshall, initializes distance matrix 
-        // for (int i = 0; i < networkWithDistance.getNumTimePoints(); i++){
-        //     outputMatrix.add(new ArrayList<Double>());  // Add list of connection weights for every time point
-        //     for (int j = 0; j < networkWithDistance.getNumTimePoints(); j++){ // check every other point for a connection
-        //         double nodeDistance = dm.get(i).get(j);
-        //         outputMatrix.get(i).add(nodeDistance);
-        //     }
-        // }
+    /**
+     * public TwoPhasePropagation(STN stn)
+     * Initializes the input STN in the class and copies the STN's current DistanceMatrix to outputMatrix.
+     * This class' functions will manipulate and return a corrected version of the STN's DistanceMatrix using 
+     * this copy.
+     * @param stn A Simple Temporal Network whose data is saved in private class variables
+     * 
+     */
+    public TwoPhasePropagation(STN stn){
+        network = stn;
+        outputMatrix = network.getDistanceMatrix();
     }
 
-    public DistanceMatrix forwardPropagate(){
-        int matrixSize = networkWithDistance.getNumTimePoints();
-        for(int i = 0; i < matrixSize; i++){ // loop through every time point
-            for(int j = 0; j < matrixSize; j++){ // check each connection to that point
-                if(outputMatrix.get(i).get(j) > networkWithDistance.getEdge(i, j).getWeight()){ // if weight of this connection less than the saved weight
+    /**
+     * public void propagate()
+     * Called once to run a Two Phase Propagation on the stored STN, loops through every time point's successors
+     * and checks their DistanceMatrix values against the edge weights between the two nodes. If it finds an
+     * inaccuracy, it calls bacPropagate to propagate the correction through the network
+     * @return outputMatrix, an up-to-date DistanceMatrix of the STN
+     * 
+     */
+    public DistanceMatrix propagate(){
+        int matrixSize = network.getNumTimePoints();
+        // loop through every node
+        for(int i = 0; i < matrixSize; i++){                                    
+            Map<Integer, Double> successorMap = network.getSuccsOf(i);
+            // check each successor to that node
+            for(Map.Entry<Integer, Double> entry : successorMap.entrySet()){    
+                int successorIndex = entry.getKey();
+                double successorWeight = entry.getValue();
+                // if weight in distance matrix is larger than weight of edge 
+                if(outputMatrix.get(i).get(successorIndex) > successorWeight){            
                     // set new distance matrix weight from the edge weight
-                    outputMatrix.get(i).set(j, networkWithDistance.getEdge(i, j).getWeight());
+                    outputMatrix.get(i).set(successorIndex, successorWeight);   
+                    // record nodes so we don't create a loop
+                    ArrayList<Integer> predecessorsChecked = new ArrayList<Integer>();
+                    // adding a node to this list as we start to backPropagate it
+                    // avoids an extra layer of function calls     
+                    predecessorsChecked.add(i);
                     // then recursively back propagate any changed nodes
-                    backPropagate(i,j); // i is nodeFrom, j is nodeTo
-
-                    // now we have checked a single successor node and ensured every predecessor is up to date
+                    backPropagate(i, successorIndex, predecessorsChecked);
+                    // now we have checked a single successor node and ensured 
+                    // every predecessor is up to date
                 }
             }
         }
-        return null;
+
+        return outputMatrix;
     }
 
-    public void backPropagate(int nodeFrom, int nodeTo){ // the edge that was just changed lead from nodeFrom to nodeTo
-        for(int i = 0; i < outputMatrix.size(); i++){ // checking every node for predecessors
-            if(outputMatrix.get(i).get(nodeFrom) != Double.POSITIVE_INFINITY){ // if we find a predecessor i->nodeFrom->nodeTo
-                // get the ideal weight of (i --> nodeTo), which is: (i --> nodeFrom) + (nodeFrom --> nodeTo)
-                double idealWeight = networkWithDistance.getEdge(i, nodeFrom).getWeight() + networkWithDistance.getEdge(nodeFrom, nodeTo).getWeight();
-                if(outputMatrix.get(i).get(nodeTo) > idealWeight){
-                    // set the weight to the better weight
-                    outputMatrix.get(i).set(nodeTo, idealWeight);
-                    // recursively back propagate
-                    backPropagate(i, nodeTo);
+    /**
+     * public void backPropagate(int nodeFrom, int nodeTo, ArrayList<Integer> predecessorsChecked)
+     * Called for every predecessor, checks the current recorded distance between
+     * nodeFrom and nodeTo and compared that to the sum of the edges connecting them.
+     * The smaller value is set as the new distance. 
+     * @param nodeFrom The predecessor node's integer index whose weight is being corrected
+     * @param nodeTo The original successor's integer index passed down by forwardPropagate
+     * @param oredecessorsChecked An ArrayList of Integers which records nodes that have been backPropagated.
+     *                            Each backPropagate "thread" has its own list to account for the possibility
+     *                            of reaching a node from two different routes.
+     */
+    public void backPropagate(int nodeFrom, int nodeTo, ArrayList<Integer> predecessorsChecked){
+        Map<Integer, Double> predecessorMap = network.getPredsOf(nodeFrom);
+        // loop through every predecessors
+        for(Map.Entry<Integer, Double> entry : predecessorMap.entrySet()){ 
+            int predecessorIndex = entry.getKey();
+            double predecessorWeight = entry.getValue();
+            // get the ideal weight of (i --> nodeTo), 
+            // which is: (i --> nodeFrom) + (nodeFrom --> nodeTo)
+            // The second value in the equation is accurate due to having 
+            // just being checked in the last function call
+            double idealWeight = predecessorWeight + outputMatrix.get(nodeFrom).get(nodeTo); 
+            // check if weight is worse than ideal for this path
+            if(outputMatrix.get(predecessorIndex).get(nodeTo) > idealWeight){ 
+                // set the weight to the better weight
+                outputMatrix.get(predecessorIndex).set(nodeTo, idealWeight);
+                // if the next predecessor has not been backPropagated yet
+                if(!predecessorsChecked.contains(predecessorIndex)){
+                    // mark that the next predecessor has been checked
+                    // adding a node to this list as we start to backPropagate it
+                    // avoids an extra layer of function calls  
+                    predecessorsChecked.add(predecessorIndex); 
+                    // recursively back propagate 
+                    backPropagate(predecessorIndex, nodeTo, predecessorsChecked);
+                } else {
+                    // let me know that this is working and not 
+                    // causing infinite loops in some corner case
+                    System.out.println("This Network has a loop, backPropagation stopped at node " +  predecessorIndex);
                 }
             }
         }
