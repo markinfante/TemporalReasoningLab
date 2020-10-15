e240085753fba98a4e5c061f31dac145d4e9814d
ckodonnell <codonnell@vassar.edu>
2020-10-15 11:22:01 -0400

    added incrementorTest to TestSuite, added new user functionality (add edge) to TempLab

--- src/TestSuite.java
+++ src/TestSuite.java
@@ -44,6 +44,28 @@ public class TestSuite {
         System.out.println(display);
     }
 
+    public void testIncrementor(Edge edge, STN tSTN){
+        String display = "";
+
+        display += tSTN.toString() + "\n";
+        try {
+            FloydWarshall fw = new FloydWarshall(tSTN);
+            tSTN.setDistanceMatrix(fw.generateMatrix());
+            display += "Old distance matrix: \n" + tSTN.getDistanceMatrix().toString();
+            // #mi IF YOU WANT TO TEST YOUR ALG MIMIC THE ABOVE HERE
+            //      vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+            NaiveAlgorithm na = new NaiveAlgorithm(tSTN);
+            tSTN.setDistanceMatrix(na.updateDistanceMatrix(edge, tSTN.getDistanceMatrix()));
+            display += "New distance matrix: \n" + tSTN.getDistanceMatrix().toString() + "\n";
+            //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        } catch (Exception e){
+            System.err.println("Failed: "+ e);
+        }
+        display += "\n========================================================\n\n";
+
+        System.out.println(display);
+    }
+
     public void runTests(){
         String display = "";
         STN tSTN = null;
