f9669b65e75292d258baf9481212d570398851c3
ckodonnell <71301178+ckodonnell@users.noreply.github.com>
2020-10-15 09:39:10 -0400

    Minor semantic changes
    Instead of getting data from the STN network, get it from the DM itself.

--- src/NaiveAlgorithm.java
+++ src/NaiveAlgorithm.java
@@ -9,26 +9,30 @@ package src;
 public class NaiveAlgorithm
 {
     private STN network;
+    private DistanceMatrix dm;
 
     public NaiveAlgorithm(STN network)
     {
         this.network = network;
     }
     
-    public void updateDistanceMatrix(Edge edge, DistanceMatrix dm)
+    public DistanceMatrix updateDistanceMatrix(Edge edge, DistanceMatrix dm)
     {
         Integer x = edge.getStart();
         Integer y = edge.getEnd();
-        network.addEdge(edge); //add edge to network graph
+        Double w = edge.getWeight();
+        network.addEdge(edge);
 
         for (int u = 0; u < dm.size(); u++){
             for (int v = 0; v < dm.size(); v++){
-                Double tempEdgeWeight = (network.getEdge(u,x).getWeight() + network.getEdge(x,y).getWeight() + network.getEdge(y,v).getWeight());
+                Double tempEdgeWeight = (dm.get(u).get(x) + w + dm.get(y).get(v));
                 //if new path cost is less than the cost in the distance matrix, update distance matrix with new cost
                 if (tempEdgeWeight < dm.get(u).get(v)) { 
                     dm.get(u).set(v, tempEdgeWeight);
                 }
             }
         }
+        return dm;
     }
 }
+
